3f1405ec121ef286c1b3f6c04b14d8d0788d6136
j6455lienv <venet.julien@gmail.com>
2022-06-28 13:57:17 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flut_app/reandomWords.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        // Add the 5 lines from here...
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const RandomWords(),
+    );
+  }
+}
